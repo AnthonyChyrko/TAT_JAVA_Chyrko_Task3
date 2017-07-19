add8132feaae8322acbd73d04efd70332f34a3e8
Anton Chyrko <Anton_Chyrko@epam.com>
2017-07-19 17:11:06 +0300

    create SAXparser

---
+++ TAT_JAVA_Chyrko_Task3/src/com/epam/library/controller/utils/parser/RunnerParser.java
@@ -0,0 +1,14 @@
+package com.epam.library.controller.utils.parser;
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
+public class RunnerParser {
+
+	public static void main(String[] args) throws SAXException, IOException {
+		new SAXparser().parseXML();
+
+	}
+
+}
